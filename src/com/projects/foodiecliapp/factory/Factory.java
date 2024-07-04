b3fa182a220457d6b39c51d33b5dc1fc2b13e836
SravanKondeti <sravankondeti16@gmail.ccom>
2024-07-05 00:28:08 +0530

    feat: Add Customer Dependencies

--- src/com/projects/foodiecliapp/factory/Factory.java
+++ src/com/projects/foodiecliapp/factory/Factory.java
@@ -6,7 +6,21 @@ import com.projects.foodiecliapp.service.CustomerServiceImpl;
 
 public class Factory {
 
-    public static CustomerRepositoryImpl customerRepository = new CustomerRepositoryImpl();
-    public static CustomerServiceImpl customerService = new CustomerServiceImpl(customerRepository);
-    public static CustomerController customerController = new CustomerController(customerService);
+    public static CustomerRepositoryImpl getCustomerRepository(){
+        return Holder.customerRepository;
+    }
+    public static CustomerServiceImpl getCustomerService(){
+        return Holder.customerService;
+    }
+    public static CustomerController getCustomerController(){
+        return Holder.customerController;
+    }
+
+    private static class Holder{
+        private static final CustomerRepositoryImpl customerRepository = new CustomerRepositoryImpl();
+        private static final CustomerServiceImpl customerService = new CustomerServiceImpl(customerRepository);
+        private static final CustomerController customerController = new CustomerController(customerService);
+    }
 }
+
+
