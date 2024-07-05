75c43de9f15ce495d0411b3e444b9e68f2d60196
SravanKondeti <sravankondeti16@gmail.ccom>
2024-07-05 13:27:11 +0530

    feat: Add Dish Service Interface & Impplementation

---
+++ src/com/projects/foodiecliapp/service/DishService.java
@@ -0,0 +1,15 @@
+package com.projects.foodiecliapp.service;
+
+import com.projects.foodiecliapp.exceptions.DishExistsException;
+import com.projects.foodiecliapp.exceptions.DishNotFoundException;
+import com.projects.foodiecliapp.model.Dish;
+
+import java.util.List;
+
+public interface DishService {
+
+    public List<Dish> getDishList();
+    public Dish save(Dish dish) throws DishExistsException;
+    public Dish getDishById(String id) throws DishNotFoundException;
+
+}
---
+++ src/com/projects/foodiecliapp/service/DishServiceImpl.java
@@ -0,0 +1,38 @@
+package com.projects.foodiecliapp.service;
+
+import com.projects.foodiecliapp.exceptions.DishExistsException;
+import com.projects.foodiecliapp.exceptions.DishNotFoundException;
+import com.projects.foodiecliapp.model.Dish;
+import com.projects.foodiecliapp.repository.DishRepositoryImpl;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DishServiceImpl implements DishService{
+
+    private DishRepositoryImpl dishRepository;
+    public DishServiceImpl(DishRepositoryImpl dishRepository) {
+        this.dishRepository = dishRepository;
+    }
+
+    @Override
+    public List<Dish> getDishList() {
+        return dishRepository.getDishList();
+    }
+
+    @Override
+    public Dish save(Dish dish) throws DishExistsException {
+        Optional<Dish> dishById = this.dishRepository.getDishById(dish.getId());
+        if(dishById.isPresent())
+            throw new DishExistsException("Dish already exists with Id: " + dish.getId());
+        return this.dishRepository.save(dish);
+    }
+
+    @Override
+    public Dish getDishById(String id) throws DishNotFoundException {
+        Optional<Dish> dishById = this.dishRepository.getDishById(id);
+        if(!dishById.isPresent())
+            throw new DishNotFoundException("Dish not found with Id: " + id);
+        return dishById.get();
+    }
+}
