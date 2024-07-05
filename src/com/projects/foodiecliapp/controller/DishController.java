cc896600bf1bd1de4cad2621ba0142d4c00655ca
SravanKondeti <sravankondeti16@gmail.ccom>
2024-07-05 13:27:31 +0530

    feat: Add Dish Controller

---
+++ src/com/projects/foodiecliapp/controller/DishController.java
@@ -0,0 +1,30 @@
+package com.projects.foodiecliapp.controller;
+
+import com.projects.foodiecliapp.exceptions.DishExistsException;
+import com.projects.foodiecliapp.exceptions.DishNotFoundException;
+import com.projects.foodiecliapp.model.Dish;
+import com.projects.foodiecliapp.service.DishServiceImpl;
+
+import java.util.List;
+
+public class DishController {
+
+    private DishServiceImpl dishService;
+
+    public DishController(DishServiceImpl dishService) {
+        this.dishService = dishService;
+    }
+
+    public List<Dish> dishList(){
+        return this.dishService.getDishList();
+    }
+
+    public Dish save(Dish dish) throws DishExistsException {
+        return this.dishService.save(dish);
+    }
+
+    public Dish getDishById(String id) throws DishNotFoundException {
+        return this.dishService.getDishById(id);
+    }
+
+}
