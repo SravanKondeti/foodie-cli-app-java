f37de8e31e86f4ef35df3e70ca74e5426834001d
SravanKondeti <sravankondeti16@gmail.ccom>
2024-07-09 00:32:52 +0530

    feat: Add Customer Section in Menu

--- src/com/projects/foodiecliapp/Main.java
+++ src/com/projects/foodiecliapp/Main.java
@@ -1,18 +1,21 @@
 package com.projects.foodiecliapp;
 
+import com.projects.foodiecliapp.controller.DishController;
+import com.projects.foodiecliapp.factory.Factory;
 import com.projects.foodiecliapp.repository.CustomerRepositoryImpl;
+import com.projects.foodiecliapp.service.DishServiceImpl;
+import com.projects.foodiecliapp.ui.Menu;
 import com.projects.foodiecliapp.util.CsvReader;
 
 public class Main {
 
     public static void main(String[] args) {
-//        CustomerRepositoryImpl customerRepository = new CustomerRepositoryImpl();
-//        System.out.println(customerRepository.getAllCustomers());
 
-        CsvReader csvReader = new CsvReader();
-        System.out.println(csvReader.readCustomersFromCsv());
-//        csvReader.readDishesFromCsv();
-//        csvReader.readRestaurantsFromCsv();
 
+//        DishController dishController  = Factory.getDishController();
+//        System.out.println(dishController.dishList());
+
+        Menu menu = new Menu();
+        menu.displayMenu();
     }
 }
