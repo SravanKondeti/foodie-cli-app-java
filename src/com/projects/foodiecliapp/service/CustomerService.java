7cfae472047ce8b38a35d700b3ca64a7defc0d40
SravanKondeti <sravankondeti16@gmail.ccom>
2024-07-09 00:28:41 +0530

    feat: Implemented CRUD methods in Service

--- src/com/projects/foodiecliapp/service/CustomerService.java
+++ src/com/projects/foodiecliapp/service/CustomerService.java
@@ -1,9 +1,19 @@
 package com.projects.foodiecliapp.service;
 
 import com.projects.foodiecliapp.exceptions.CustomerExistsException;
+import com.projects.foodiecliapp.exceptions.CustomerNotFoundException;
 import com.projects.foodiecliapp.model.Customer;
+
+import java.util.List;
 
 public interface CustomerService {
 
     public Customer save(Customer customer) throws CustomerExistsException;
+    public List<Customer> getCustomerList();
+    public Customer getCustomerById(String Id) throws CustomerNotFoundException;
+    public Customer updateCustomer(Customer customer) throws CustomerNotFoundException;
+    public void deleteCustomer(String Id) throws CustomerNotFoundException;
+    public Customer validateCustomerLogin(String email, String password) throws CustomerNotFoundException;
+    public void setCurrentLoggedInCustomer(Customer customer);
+    public Customer getCurrentLoggedInCustomer();
 }
--- src/com/projects/foodiecliapp/service/CustomerServiceImpl.java
+++ src/com/projects/foodiecliapp/service/CustomerServiceImpl.java
@@ -1,14 +1,17 @@
 package com.projects.foodiecliapp.service;
 
 import com.projects.foodiecliapp.exceptions.CustomerExistsException;
+import com.projects.foodiecliapp.exceptions.CustomerNotFoundException;
 import com.projects.foodiecliapp.model.Customer;
 import com.projects.foodiecliapp.repository.CustomerRepositoryImpl;
 
+import java.util.List;
 import java.util.Optional;
 
 public class CustomerServiceImpl implements CustomerService{
 
     private CustomerRepositoryImpl customerRepository;
+    private Customer currentLoggedInCustomer;
 
     public CustomerServiceImpl(CustomerRepositoryImpl customerRepository) {
         this.customerRepository = customerRepository;
@@ -21,4 +24,52 @@ public class CustomerServiceImpl implements CustomerService{
             throw new CustomerExistsException("Customer already exists with this ID: " + customer.getCustomerId());
         return this.customerRepository.save(customer);
     }
+
+    @Override
+    public List<Customer> getCustomerList() {
+        return customerRepository.getAllCustomers();
+    }
+
+    @Override
+    public Customer getCustomerById(String Id) throws CustomerNotFoundException {
+        Optional<Customer> customerById = this.customerRepository.getCustomerById(Id);
+        if(customerById.isEmpty())
+            throw new CustomerNotFoundException("Customer already exists with this ID: " + Id);
+        return customerById.get();
+    }
+
+    @Override
+    public Customer updateCustomer(Customer customer) throws CustomerNotFoundException {
+        Optional<Customer> customerById = this.customerRepository.getCustomerById(customer.getCustomerId());
+        if(customerById.isEmpty())
+            throw new CustomerNotFoundException("Customer not found with Id : " + customer.getCustomerId());
+        return this.customerRepository.updateCustomer(customer);
+    }
+
+    @Override
+    public void deleteCustomer(String Id) throws CustomerNotFoundException {
+        Optional<Customer> customerById = this.customerRepository.getCustomerById(Id);
+        if(customerById.isEmpty())
+            throw new CustomerNotFoundException("Customer not found with Id : " + Id);
+        this.customerRepository.deleteCustomer(customerById.get());
+    }
+
+    @Override
+    public Customer validateCustomerLogin(String email, String password) throws CustomerNotFoundException {
+        Optional<Customer> customer = this.customerRepository.findByEmailAndPassword(email, password);
+        if(customer.isEmpty())
+            throw new CustomerNotFoundException("Invalid Email or Password");
+        return customer.get();
+    }
+
+    @Override
+    public void setCurrentLoggedInCustomer(Customer customer) {
+        this.currentLoggedInCustomer = customer;
+    }
+
+    @Override
+    public Customer getCurrentLoggedInCustomer() {
+        return this.currentLoggedInCustomer;
+    }
+
 }
