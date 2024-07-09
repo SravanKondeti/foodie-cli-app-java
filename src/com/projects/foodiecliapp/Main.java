35cc88a8d907799205c5aab993f21c94322f46a0
SravanKondeti <sravankondeti16@gmail.ccom>
2024-07-09 19:39:53 +0530

    feat: Test CRUD methods in Menu

--- src/com/projects/foodiecliapp/Main.java
+++ src/com/projects/foodiecliapp/Main.java
@@ -1,18 +1,13 @@
 package com.projects.foodiecliapp;
 
 import com.projects.foodiecliapp.repository.CustomerRepositoryImpl;
+import com.projects.foodiecliapp.ui.Menu;
 import com.projects.foodiecliapp.util.CsvReader;
 
 public class Main {
 
     public static void main(String[] args) {
-//        CustomerRepositoryImpl customerRepository = new CustomerRepositoryImpl();
-//        System.out.println(customerRepository.getAllCustomers());
-
-        CsvReader csvReader = new CsvReader();
-        System.out.println(csvReader.readCustomersFromCsv());
-//        csvReader.readDishesFromCsv();
-//        csvReader.readRestaurantsFromCsv();
-
+        Menu menu = new Menu();
+        menu.displayMenu();
     }
 }
